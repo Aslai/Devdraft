1a3f114b99719b0047f395586d94aa6df9f0dc13
Kaslai <kaslai@kaslai.com>
2014-09-29 13:58:22 -0700

    Complete Challenge #2

--- part2.cpp
+++ part2.cpp
@@ -1,107 +1,143 @@
 #include<cstdio>
-#include<cstdlib>
-#include<string>
-#include<cinttypes>
 #include<vector>
-
+#include<map>
 
 #define USE_FILESTREAMS
 
-struct Field{
-    struct Coin{
+//!This code is a lot more straightforward that the entry I submitted for the first challenge.
+//!I was looking at the patterns generated with the laser in this problem, and it appears that an O(n)
+//!algorithm is possible, where n is the number of coins. After a few hours of testing, I couldn't
+//!derive the patterns mathematically, so I opted for a more obvious approach. With more diligence,
+//!I imagine the patterns could be derived mathematically.
+
+struct Field {
+    struct Coin {
         int x, y, value;
+        int touch_x, touch_y, touch_t;
+        bool check_x, check_y, check_d1, check_d2;
     };
     int width;
     int height;
     std::vector<Coin> coins;
+    std::map<int,Coin> coinmap;
 };
 
-struct Speed{
+struct Speed {
     int x, y;
 };
 
-int compute( Field f ){
-    int major = f.width;
-    int minor = f.height;
-    if( minor > major ){
-        major = f.height;
-        minor = f.width;
-    }
-
-    std::vector<int> classes(minor, 0);
-    int classnumber = 1;
-
-    for( int xstart = 0; xstart < minor; ++xstart ){
-        if( classes[xstart] != 0 )
-            continue;
-        Speed s = {1, 1};
-        if( minor == 1 ){
-            s.y = 0;
-        }
-        if( major == 1 ){
-            s.x = 0;
-        }
-
-        Speed start = s;
-        int x = xstart;
-        int y = 0;
-        int step = 0;
-        do{
-            ++step;
-            if( y == 0 && classes[x] > 0 ){
-                classes[xstart] = classes[x];
-            }
-            x += s.x;
-            if( x <= 0 || x >= minor-1 ){ //Bounce off of vertical walls
-                s.x = -s.x;
-            }
-            y += s.y;
-            if( y < 0 || y >= major ){ //Bounce off of horizontal walls
-                s.y = -s.y;
-            }
-        } while(    step < 5 ||
-                  !(x == xstart && //While not back at the start with the starting speed
-                    y == 0 &&
-                    s.x == start.x &&
-                    s.y == start.y) );
-
-        if( classes[xstart] == 0 ){
-            classes[xstart] = classnumber++;
-        }
-    }
-    for( int i = 0; i < classes.size(); ++i ){
-        printf("%d\t", classes[i] );
-    }
-    printf("\n");
+int hash( int x, int y, int width ) { //This is much faster than using std::pair in std::map
+    return x + y * width * 2;
 }
 
-int main(){
+int maxpath( Field &f, Field::Coin c ) {
+    int ret = 0;
+    int trackx = 0, tracky = 0;
+    if( c.check_x == false || c.check_y == false ) { //If this coin has been checked by another coin, don't recheck it.
+        for( size_t i = 0; i < f.coins.size(); ++i ) { //Determine the value of straight vertical or horizontal shots.
+            if( f.coins[i].x == c.x ) {
+                trackx += f.coins[i].value;
+                f.coins[i].check_x = true; //Prevent checking this coin redundantly
+            }
+            if( f.coins[i].y == c.y ) {
+                tracky += f.coins[i].value;
+                f.coins[i].check_y = true; //Prevent checking this coin redundantly
+            }
+        }
+    }
+    ret = tracky;
+    if( trackx > tracky) {
+        ret = trackx;
+    }
+    if( f.width > 1 && f.height > 1 ) { //Don't check diagonals if there's no need to
+        for( int trial = -1; trial <= 1; trial += 2 ) { //check both diagonals
+            int amount = 0;
+            Speed s = { trial, 1 };
+            int x = c.x;
+            int y = c.y;
+            int passes = 0;
+            do {
+                if( x == c.x && y == c.y ) { //This keeps track of how many times the origin has been passed.
+                    ++passes;
+                }
+                if( f.coinmap.find( hash( x, y, f.width ) ) != f.coinmap.end() ) { //Prevent adding empty entries into the map
+                    Field::Coin &test_coin = f.coinmap[hash( x, y, f.width )];
+
+                    if( !( test_coin.touch_x == c.x && test_coin.touch_y == c.y && test_coin.touch_t == trial ) ) {
+                        //If we haven't already visited this coin during this trial, mark it as having been visited.
+                        test_coin.touch_x = c.x;
+                        test_coin.touch_y = c.y;
+                        test_coin.touch_t = trial;
+                        amount += test_coin.value;
+                        if( s.x == -1 ) {
+                            test_coin.check_d1 = true; //Prevent checking this coin redundantly
+                        } else {
+                            test_coin.check_d2 = true; //Prevent checking this coin redundantly
+                        }
+                    }
+                }
+
+                //Handle laser reflection
+                if( (x <= 0 && s.x < 0) || (x >= f.width && s.x > 0) ) {
+                    s.x = -s.x;
+                }
+                if( (y <= 0 && s.y < 0) || (y >= f.height && s.y > 0) ) {
+                    s.y = -s.y;
+                }
+                x += s.x;
+                y += s.y;
+            } while( passes <= 2 );
+
+            if( amount > ret ) {
+                ret = amount;
+            }
+        }
+    }
+    return ret;
+}
+
+int compute( Field f ) {
+    int maxval = 0;
+    for( size_t i = 0; i < f.coins.size(); ++i ) {
+        int value = maxpath( f, f.coins[i] );
+        if( value > maxval ) {
+            maxval = value;
+        }
+    }
+    return maxval;
+}
+
+int main() {
     #ifdef USE_FILESTREAMS
     freopen( "input.txt", "r", stdin );
     freopen( "output.txt", "w", stdout );
     #endif
-    while( !feof( stdin ) ){
+
+    while( !feof( stdin ) ) {
         Field f;
         Field::Coin c;
         int coins;
         int result = fscanf( stdin, "%d %d %d", &f.width, &f.height, &coins );
-        if( result != 3 ){
+        if( result != 3 ) {
             fprintf( stderr, "Failed to parse values.\n" );
             continue;
         }
-        while( coins-- > 0 ){
+        while( coins-- > 0 ) {
             result = fscanf( stdin, "%d %d %d", &c.x, &c.y, &c.value );
-            if( result != 3 ){
+            c.check_x = c.check_y = c.check_d1 = c.check_d2 = false;
+            c.touch_t = c.touch_x = c.touch_y = -10;
+            if( result != 3 ) {
                 fprintf( stderr, "Failed to parse values.\n" );
                 break;
             }
             f.coins.push_back( c );
+            f.coinmap[hash( c.x, c.y, f.width )] = c;
         }
-        if( coins > 0 ){
+        if( coins > 0 ) {
             fprintf( stderr, "Too many coins specified.\n" );
             continue;
         }
-        compute( f );
+        printf("%d\n", compute( f ) );
 
     }
     return 0;
