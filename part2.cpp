607fc77f45876dd3c7eeeb9cb5ea69bb4a79d69f
Kaslai <kaslai@kaslai.com>
2014-09-29 10:18:12 -0700

    Get reading of values working

--- part2.cpp
+++ part2.cpp
@@ -4,91 +4,54 @@
 #include<cinttypes>
 #include<vector>
 
-#pragma pack(1)
-struct BitmapHeader{
-    char magic[2];          //must be BM
-    uint32_t filesize;
-    uint16_t reserved1;
-    uint16_t reserved2;
-    uint32_t dataoffset;    //must be sizeof(Bitmapheader)
-    uint32_t dibsize;       //must be 12
-    uint16_t width;
-    uint16_t height;
-    uint16_t colorplanes;   //Must be 1
-    uint16_t bpp;
+
+#define USE_FILESTREAMS
+
+struct Field{
+    struct Coin{
+        int x, y, value;
+    };
+    int width;
+    int height;
+    std::vector<Coin> coins;
 };
 
-enum class Slope{
-    Positive,
-    Negative
-};
-
-void generatebmp( std::string file, int width, int height, int x, int y, Slope s ){
-    std::vector<bool> pixels(width * height, false);
-    struct{
-        int x, y;
-    }speed = {1, 1};
-
-    if( s == Slope::Negative ){
-        speed.y = -1;
+int compute( Field f ){
+    printf("%d %d %d\n", f.width, f.height, f.coins.size() );
+    for( int i = 0; i < f.coins.size(); ++i ){
+        printf("%d %d %d\n", f.coins[i].x, f.coins[i].y, f.coins[i].value);
     }
-
-    for( int i = 0; i < width * height; ++i ){
-        pixels[x + y * width] = true;
-        x += speed.x;
-        if( x >= width || x < 0 ){
-            x -= 2 * speed.x;
-            speed.x = -speed.x;
-        }
-        y += speed.y;
-        if( y >= height || y < 0 ){
-            y -= 2 * speed.y;
-            speed.y = -speed.y;
-        }
-    }
-
-    int widthpadding = (4 - width * 3 % 4) % 4;
-    BitmapHeader head;
-    head.magic[0] = 'B';
-    head.magic[1] = 'M';
-    head.filesize = sizeof( BitmapHeader ) + (width*3 + widthpadding) * height;
-    head.dataoffset = sizeof( BitmapHeader );
-    head.dibsize = 12;
-    head.width = width;
-    head.height = height;
-    head.colorplanes = 1;
-    head.bpp = 24;
-
-
-    FILE* f = fopen( file.c_str(), "wb" );
-    fwrite( &head, 1, sizeof( BitmapHeader), f );
-
-    for( size_t i = 0; i < pixels.size(); ++i ){
-        if( pixels[i] ){
-            fputc( 0, f );
-            fputc( 0, f );
-            fputc( 0, f );
-        }
-        else{
-            fputc( 255, f );
-            fputc( 255, f );
-            fputc( 255, f );
-        }
-        if( i % width == width - 1 ){
-            for( int j = 0; j < widthpadding; ++j ){
-                fputc(0, f);
-            }
-        }
-    }
-    fclose( f );
+    printf("\n");
 }
 
 int main(){
-    for( int i = 0; i < 40; ++i ){
-        for( int j = 0; j < 40; ++j ){
-            char str[100];
-            sprintf( str, "ss/img-%d-%d.bmp", i, j );
-            generatebmp( str, i, j, 0, 0, Slope::Positive );
+    #ifdef USE_FILESTREAMS
+    freopen( "input.txt", "r", stdin );
+    freopen( "output.txt", "w", stdout );
+    #endif
+    while( !feof( stdin ) ){
+        Field f;
+        Field::Coin c;
+        int coins;
+        int result = fscanf( stdin, "%d %d %d", &f.width, &f.height, &coins );
+        if( result != 3 ){
+            fprintf( stderr, "Failed to parse values.\n" );
+            continue;
         }
+        while( coins-- > 0 ){
+            result = fscanf( stdin, "%d %d %d", &c.x, &c.y, &c.value );
+            if( result != 3 ){
+                fprintf( stderr, "Failed to parse values.\n" );
+                break;
+            }
+            f.coins.push_back( c );
+        }
+        if( coins > 0 ){
+            fprintf( stderr, "Too many coins specified.\n" );
+            continue;
+        }
+        compute( f );
+
     }
+    return 0;
 }
